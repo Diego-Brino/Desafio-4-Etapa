81dcd766d89ffd3fc9e324c149a90f8185a5826b
Diego Brino <diego.simonaio@hotmail.com>
2022-09-11 23:01:32 -0300

    Configurações iniciais de projeto.

---
+++ backend/scilink/src/main/java/com/api/scilink/ScilinkApplication.java
@@ -0,0 +1,13 @@
+package com.api.scilink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScilinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScilinkApplication.class, args);
+	}
+
+}
---
+++ backend/scilink/src/test/java/com/api/scilink/ScilinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.scilink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScilinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
